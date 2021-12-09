6b9117617ef2ad04e2199488d2d639eafe4d8b5a
Alex-Reyne <creativereyne@gmail.com>
2021-12-08 19:52:13 -0600

    create InterviewerList and accompanying styles

---
+++ src/components/InterviewerList.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import InterviewerListItem from "./InterviewerListItem";
+import 'components/InterviewerList.scss'
+
+export default function InterviewerList(props) {
+  const { interviewers, onChange, value } = props;
+  console.log(props)
+
+  const interviewerList = interviewers.map((item) => {
+    const { id, name, avatar } = item;
+
+    return <InterviewerListItem
+      key={ id }
+      name={ name }
+      avatar={ avatar }
+      selected={ value === id }
+      onChange={ (e) => onChange(id) }
+    />
+  })
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        { interviewerList }
+      </ul>
+    </section>
+  );
+
+};
