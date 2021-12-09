d5b4c9bd4a0d33f2080f064a914b333127a05733
Alex-Reyne <creativereyne@gmail.com>
2021-12-08 19:55:26 -0600

    create appointment components. (Confirm, empty, error, form, header, show, status, index). Add Styles to appointments.

---
+++ src/components/Appointment/Form.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Form(props) {
+  const { student, interviewers, interviewer, onSave, onCancel } = props;
+
+  return (
+
+  );
+}
---
+++ src/components/Appointment/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './styles.scss';
+
+export default function Appointment(props) {
+
+  return (
+    <article className='appointment'>
+      { !props.time && 'No Appointments'}
+      { props.time && `Appointment at ${props.time}`}
+    </article>
+  );
+}
