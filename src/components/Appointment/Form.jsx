f4914778b552380faf8e3fbb13cd1de9246f7c47
Alex-Reyne <creativereyne@gmail.com>
2021-12-08 22:12:29 -0600

    completed Form.jsx

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -3,16 +3,25 @@ import Button from "components/Button";
 import InterviewerList from "components/InterviewerList";
 
 export default function Form(props) {
-  console.log(props)
   const [student, setStudent] = useState(props.student || '');
   const [interviewer, setInterviewer] = useState(props.interviewer || null)
-  const { interviewers, onSave, onCancel, onChange } = props;
+  const { interviewers, onSave, onCancel } = props;
+
+  const reset = () => {
+    setStudent('');
+    setInterviewer('');
+  }
+
+  const cancel = () => {
+    onCancel();
+    reset();
+  }
 
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
@@ -26,12 +35,11 @@ export default function Form(props) {
           interviewers={ interviewers }
           onChange={ setInterviewer }
           value={ interviewer }
-          onChange={ setInterviewer }
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={ onCancel } >Cancel</Button>
+          <Button danger onClick={ cancel } >Cancel</Button>
           <Button confirm onClick={ onSave } >Save</Button>
         </section>
       </section>
