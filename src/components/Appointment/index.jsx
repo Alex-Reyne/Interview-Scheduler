af7024944c44b0865bc71d27ad190897a2c23fae
Alex-Reyne <creativereyne@gmail.com>
2021-12-10 19:18:32 -0600

    create useVisualMode function

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -2,18 +2,34 @@ import React from "react";
 import Show from "./Show";
 import Header from "./Header";
 import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode";
 import './styles.scss';
+
+const EMPTY = 'EMPTY';
+const SHOW = 'SHOW';
+const CREATE = 'CREATE';
 
 export default function Appointment(props) {
   const { id, time, interview } = props;
+  
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  )
 
   return (
     <article className='appointment'>
       <Header time={ time } />
-      { interview ? 
-        <Show {...interview} />
-      : 
-        <Empty />}
+      { mode === EMPTY && <Empty onAdd={() => transition(CREATE)} /> } 
+      { mode === SHOW && (
+        <Show 
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      { mode === CREATE &&
+        <Show
+        
+        /> }
     </article>
   );
 }
