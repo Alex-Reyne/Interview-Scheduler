b0c1a749d711fc7cef1cda2c459a6325450f665a
Alex-Reyne <creativereyne@gmail.com>
2021-12-15 07:58:29 -0600

    create first round of Form tests

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -18,30 +18,35 @@ export default function Form(props) {
   }
 
   return (
-    <main className="appointment__card appointment__card--create">
-      <section className="appointment__card-left">
-        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
-          <input
-            className="appointment__create-input text--semi-bold"
-            name="name"
-            type="text"
-            placeholder="Enter Student Name"
-            value={ student }
-            onChange={(e) => setStudent(e.target.value)}
-          />
-        </form>
-        <InterviewerList
-          interviewers={ interviewers }
-          onChange={ setInterviewer }
-          value={ interviewer }
-        />
-      </section>
-      <section className="appointment__card-right">
-        <section className="appointment__actions">
-          <Button danger onClick={ cancel } >Cancel</Button>
-          <Button confirm onClick={ () => onSave(student, interviewer) } >Save</Button>
-        </section>
-      </section>
-    </main>
-  );
+		<main className='appointment__card appointment__card--create'>
+			<section className='appointment__card-left'>
+				<form autoComplete='off' onSubmit={(event) => event.preventDefault()}>
+					<input
+						className='appointment__create-input text--semi-bold'
+						name='name'
+						type='text'
+						placeholder='Enter Student Name'
+						value={ student }
+						onChange={(e) => setStudent(e.target.value)}
+						data-testid='student-name-input'
+					/>
+				</form>
+				<InterviewerList
+					interviewers={interviewers}
+					onChange={setInterviewer}
+					value={interviewer}
+				/>
+			</section>
+			<section className='appointment__card-right'>
+				<section className='appointment__actions'>
+					<Button danger onClick={cancel}>
+						Cancel
+					</Button>
+					<Button confirm onClick={() => onSave(student, interviewer)}>
+						Save
+					</Button>
+				</section>
+			</section>
+		</main>
+	);
 }
